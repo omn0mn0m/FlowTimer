177a3b1acd9b00280af452b9805943b904e3f6fd
omn0mn0m <tech@omn0mn0m.com>
2024-12-18 15:40:53 -0500

    Added pomo functionality

--- config.js
+++ config.js
@@ -3,12 +3,18 @@ const config = {
   "twitchBot": "", // bot username
   "oauth": "", // "oauth:whatever"
   "focusRatio": 5, // 5 = 50 minutes of focus, 10 minutes of break
-  "autostartFocusAfterBreak": false, // TODO make this do something
+  "autostartFocusAfterBreak": true, // whether to start the next focus after break
   "showHours": false, // false = 75:00, true = 1:15:00
   "mainCommand": "timer", // the first part of command i.e. !timer pause
+  "defaultMode": "Pomodoro", // either "Flowtime" or "Pomodoro"
   "resourcePaths": {
     "focusSoundPath": "res/workSound.riff", // sound that plays on !timer focus command
     "breakSoundPath": "res/breakSound.riff" // sound that plays on !timer break command
+  },
+  "pomodoro": {
+    "focusDuration": 50, // time in minutes
+    "breakDuration": 10, // time in minutes
+    "goal": 5
   },
   "messages": {
     "startFocus": "Starting the focus timer. See you during the break!",
--- js/timer.js
+++ js/timer.js
@@ -1,27 +1,41 @@
-let focusCount = 0;
-let focusDuration = 0;
-let focusRatio = config.focusRatio;
-let breakCount = 0;
-let breakDuration = 0;
-
-let startTime;
-let endTime;
-let pauseTime;
-
-let totalFocusTime = 0;
-
-let stopwatch;
-let timer;
-
+// Resources
 let breakSound = new Audio(config.resourcePaths.breakSoundPath);
 let focusSound = new Audio(config.resourcePaths.focusSoundPath);
 
-const FlowState = {
-  Focus: 'Focus',
-  Break: 'Break'
+// Pomodoro variables
+let pomoFocusDuration = config.pomodoro.focusDuration * 60000;
+let pomoBreakDuration = config.pomodoro.breakDuration * 60000;
+let pomoGoal = config.pomodoro.goal
+
+// Flowtime variables
+let focusRatio = config.focusRatio;
+
+// Clock variables
+let clock;
+
+let focusCount = 0;
+let breakCount = 0;
+
+let startTime;
+let pauseTime;
+
+let totalFocusTime = 0;
+let timeElapsed = 0;
+let countdownDuration = 0;
+
+const Mode = {
+  Pomodoro: 'Pomodoro',
+  Flowtime: 'Flowtime'
 };
 
-let flowState;
+let mode = config.defaultMode;
+
+const State = {
+  Focus: 'Focus',
+  Break: 'Break',
+};
+
+let state;
 
 document.addEventListener("DOMContentLoaded", function(){
   document.getElementById('time').innerHTML = config.showHours ? "00:00:00" : "00:00";
@@ -30,32 +44,55 @@ document.addEventListener("DOMContentLoaded", function(){
 ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
   if( flags.broadcaster && command == config.mainCommand ) {
     switch (message) {
-      case "focus":
-        clearInterval(timer);
-        startTime = Date.now();
-        runStopwatch();
-        ComfyJS.Say(config.messages.startFocus);
-        flowState = FlowState.Focus;
-
-        focusCount++;
+      case "flowmode":
+        mode = Mode.Flowtime;
         document.getElementById("phase").innerText = "Focus #" + focusCount;
+
+        if (state == State.Focus) {
+          setTimeDisplay(timeElapsed);
+        }
+        break;
+      case "pomomode":
+        mode = Mode.Pomodoro;
+        document.getElementById("phase").innerText = "Pomo " + focusCount + "/" + pomoGoal;
+        break;
+      case "start":
+      case "focus":
+        clearInterval(clock);
+        state = State.Focus;
+        focusCount++;
+        focusSound.play();
+        ComfyJS.Say(config.messages.startFocus);
+        startTime = Date.now();
+        
+        if (mode == Mode.Flowtime) {
+          document.getElementById("phase").innerText = "Focus #" + focusCount;
+        } else if (mode == Mode.Pomodoro) {
+          countdownDuration =  pomoFocusDuration;
+          document.getElementById("phase").innerText = "Pomo " + focusCount + "/" + pomoGoal;
+        }
+
+        runClock();
         break;
       case "break":
-        clearInterval(stopwatch);
-        let breakTime = focusDuration / focusRatio;
-        endTime = Date.now() + breakTime;
-        runTimer();
+        clearInterval(clock);
+        state = State.Break;
+        
+        if (mode == Mode.Flowtime) {
+          countdownDuration = timeElapsed / focusRatio;
+        } else if (mode == Mode.Pomodoro) {
+          countdownDuration = pomoBreakDuration;
+        }
+
+        startTime = Date.now();
         breakSound.play();
         ComfyJS.Say(config.messages.startBreak);
-        flowState = FlowState.Break;
-
         breakCount++;
-        totalFocusTime += focusDuration;
         document.getElementById("phase").innerText = "Break #" + breakCount;
-        focusDuration = 0;
+        runClock();
         break;
       case "check":
-        const calculatedFocusTime = (totalFocusTime + focusDuration) / 1000;
+        const calculatedFocusTime = totalFocusTime / 1000;
         const hours = Math.floor(calculatedFocusTime / 3600);
         const minutes = Math.floor((calculatedFocusTime % 3600) / 60);
         const seconds = Math.floor((calculatedFocusTime % 3600) % 60);
@@ -63,65 +100,84 @@ ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
         ComfyJS.Say(`${config.twitchStreamer} has focused a total of ${hours} hour(s), ${minutes} minute(s), and ${seconds} second(s).`);
         break;
       case "reset":
-        if (flowState == FlowState.Focus) {
-          startTime = Date.now();
-        } else if (flowState == FlowState.Break) {
-          let breakTime = focusDuration / focusRatio;
-          endTime = Date.now() + breakTime;
-        } else {}
+        pauseTime = 0;
         
-        setTimeDisplay(0);
+        if (state == State.Focus) {
+          startTime = Date.now();
+
+          if (mode == Mode.Flowtime) {
+            setTimeDisplay(0);
+          } else if (mode == Mode.Pomodoro) {
+            setTimerDisplay(pomoFocusDuration);
+          }
+        } else if (state == State.Break) {
+          if (mode == Mode.Flowtime) {
+            countdownDuration = timeElapsed / focusRatio;
+          } else if (mode == Mode.Pomodoro) {
+            countdownDuration = pomoBreakDuration;
+          }
+
+          setTimerDisplay(countdownDuration);
+        }
         break;
       case "pause":
         pauseTime = Date.now();
-        
-        if (flowState == FlowState.Focus) {
-          clearInterval(stopwatch);
-        } else if (flowState == FlowState.Break) {
-          clearInterval(timer);
-        } else {}
+        clearInterval(clock);
         break;
+      case "unpause":
       case "resume":
         const offset = Date.now() - pauseTime;
-        
-        if (flowState == FlowState.Focus) {
-          startTime += offset;
-          runStopwatch();
-        } else if (flowState == FlowState.Break) {
-          endTime += offset;
-          runTimer();
-        } else {}
+        startTime += offset;
+        runClock();
         break;
     }
   }
 }
 ComfyJS.Init(config.twitchBot, config.oauth, config.twitchStreamer);
 
-
-function runStopwatch() {
+function runClock() {
   const timeNow = Date.now();
-  focusDuration = timeNow - startTime;
+  timeElapsed = timeNow - startTime;
 
-  setTimeDisplay(focusDuration);
-  stopwatch = setTimeout(runStopwatch, 500);
-}
+  if (state == State.Focus) {
+    totalFocusTime += 500; // not exact due to basing time off CPU count
+  }
 
-function runTimer() {
-  const timeNow = Date.now();
-  breakDuration = endTime - timeNow;
-  
-  if (breakDuration <= 0) {
-    setTimeDisplay(0);
-    clearInterval(timer);
-    focusSound.play();
-    ComfyJS.Say(config.messages.breakOver);
+  if (state == State.Break || mode == Mode.Pomodoro) {
+    // Timer counts down
+    const countdown = countdownDuration - timeElapsed;
 
-    if (config.autostartFocusAfterBreak) {
-      // TODO autostart next focus
+    if (countdown <= 0) {
+      // Stop the clock
+      clearInterval(clock);
+      focusSound.play();
+      ComfyJS.Say(config.messages.breakOver);
+      setTimeDisplay(0);
+      
+      if (config.autostartFocusAfterBreak) {
+        state = State.Focus;
+        focusCount++;
+        focusSound.play();
+        ComfyJS.Say(config.messages.startFocus);
+        startTime = Date.now();
+        
+        if (mode == Mode.Flowtime) {
+          document.getElementById("phase").innerText = "Focus #" + focusCount;
+        } else if (mode == Mode.Pomodoro) {
+          countdownDuration =  pomoFocusDuration;
+          document.getElementById("phase").innerText = "Pomo " + focusCount + "/" + pomoGoal;
+        }
+
+        clock = setTimeout(runClock, 500);
+      }
+    } else {
+      setTimeDisplay(countdown);
+      clock = setTimeout(runClock, 500);
     }
   } else {
-    setTimeDisplay(breakDuration);
-    timer = setTimeout(runTimer, 500);
+    // Timer counts up
+    setTimeDisplay(timeElapsed);
+    clock = setTimeout(runClock, 500);
   }
 }
 
